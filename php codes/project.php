1210c2a2c7c4dfab5c92f344cde1be4469e8585b
Aleks1811 <Aleksmladenov1811@gmail.com>
2021-01-16 12:56:02 +0200

    Project
    second

--- php codes/project.php
+++ php codes/project.php
@@ -1,63 +1,33 @@
 <?php
 
-
-function checking_the_questionmark($question){
-    //$new_question = str_split($question);
-  // echo "<pre>";
-  // var_dump($new_question);
-  // echo "</pre>";
-    //$count_question = count($new_question);
-//$new_question[$count_question-1]
-    if ( (strlen($question)-1 ) == "?") {
-	     echo $question ;	
+$question = "hbhddb?";
+function creating_question_correcting_bot($questiobot){
+    if (ctype_upper($question)) {
+      echo trim(ucfirst(strtolower($question)));
+    }else{
+      echo trim(ucfirst($question));
     }
-}
 
-//IT WAS PART OF THE IF
-//end($new_question)
+    $new_question = str_split($question);
+      $count_question = count($new_question);
 
-function corect_capital_letters($question){
-     ctype_upper($question);
-     //return ($question);
+     if ( $new_question[$count_question-1] == "?") {
+   //     //echo $questions;  
+     }
+
+      // ctype_upper($questions);
+      // return ($questions);
   
-}
+      $stripped = preg_replace('/\s+/', ' ', $question);
+      return trim($stripped);
 
-function correctSpacing($question) {
-     $stripped = preg_replace('/\s+/', ' ', $question);
-     return trim($stripped);
+      $new_question = str_split($question);
+      $comma = ",";
+      $space = ' ';
 
-}
-
-function correcting_commas($question){
-     $new_question = str_split($question);
-     $comma = ",";
-     $space = ' ';
-  // echo "<pre>";
-  // var_dump($new_question);
-  // echo "</pre>";
-    if(in_array(",", $new_question)){
-       echo $comma . $space;
-    }
+     if(in_array(",", $new_question)){
+        echo $comma . $space;
+     }
 
 }
-$question = "jnj,fn fn?";
-echo checking_the_questionmark($question);
-// $question1 = "dbhddj";
-// corect_capital_letters($question1);
- //echo $question;
-
-
-//$question_mark = "!";
-//$question = $question_mark;
-
-//$final_result = $question . $question_mark;
-
-//$new_question_mark = str_split($question_mark);
-
-//var_dump($new_question_mark);
-
-//$count_mark = count($new_question_mark);
-
-// if ($count_mark == 1) {
-// 	echo $final_result;
-// }
+creating_question_correcting_bot($question);
