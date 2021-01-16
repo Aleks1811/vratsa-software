728d27a04d46747583600833ff378b7151162a25
Aleks1811 <Aleksmladenov1811@gmail.com>
2021-01-16 12:58:55 +0200

    Project
    third

--- php codes/project.php
+++ php codes/project.php
@@ -1,23 +1,11 @@
 <?php
 
-$question = "hbhddb?";
-function creating_question_correcting_bot($questiobot){
-    if (ctype_upper($question)) {
-      echo trim(ucfirst(strtolower($question)));
-    }else{
-      echo trim(ucfirst($question));
-    }
-
+//$input = "hbhddb?";
+function creating_question_correcting_bot($question){
+    
     $new_question = str_split($question);
       $count_question = count($new_question);
 
-     if ( $new_question[$count_question-1] == "?") {
-   //     //echo $questions;  
-     }
-
-      // ctype_upper($questions);
-      // return ($questions);
-  
       $stripped = preg_replace('/\s+/', ' ', $question);
       return trim($stripped);
 
@@ -25,9 +13,37 @@ function creating_question_correcting_bot($questiobot){
       $comma = ",";
       $space = ' ';
 
-     if(in_array(",", $new_question)){
-        echo $comma . $space;
-     }
+      $flag = true;
+
+
+    if (ctype_upper($question)) {
+      trim(ucfirst(strtolower($question)));
+      $flag = true;
+    }else{
+      trim(ucfirst($question));
+      $flag = false;
+      //break;
+    }
+
+    if ( $new_question[$count_question-1] == "?") {
+   //     //echo $questions;
+   $flag = true;
+    }
+
+      // ctype_upper($questions);
+      // return ($questions);
+
+    // if(in_array(",", $new_question)){
+    //     echo $comma . $space;
+    // }
+
+    if ($flag == true) {
+      echo trim(ucfirst(strtolower($question))); 
+      echo $question;
+    }else{
+      echo trim(ucfirst($question));
+    }
 
 }
-creating_question_correcting_bot($question);
+$input = "dndbd?";
+echo creating_question_correcting_bot($input);
